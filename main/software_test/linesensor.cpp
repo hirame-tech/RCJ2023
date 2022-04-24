59362f2c7f1c28a210bb583ba7cdbd03751de7ab
Slicer00 <104005167+Slicer00@users.noreply.github.com>
2022-04-24 21:19:02 +0900

    Create linesensor.cpp
    my first github upload
    nice to meet you :)

---
+++ main/software_test/linesensor.cpp
@@ -0,0 +1,21 @@
+//とりあえずディレクトリを作ってファイルを入れるテスト
+//ここだとビルド時に影響しないか不安だから適当に動かしてもらっておｋ
+
+#include<Arduino.h>
+
+int val = [30]
+
+const int censorPIN = 13  //任意のポート番号
+
+void setup(){
+  pinMode(censorPIN,INPUT);
+  Serial.begin(115200);
+  
+}
+
+void loop(){
+  val[0] = analogRead(censorPIN);
+  Serial.println(val[0]);
+  delay(100);
+
+}
