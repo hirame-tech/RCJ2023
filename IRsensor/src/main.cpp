25617c01ec2412783e7912c16187779bf4a50af7
hamakawa <hirame.tech@gmail.com>
2022-05-19 11:53:18 +0900

    Commit

---
+++ IRsensor/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+int pin[]={
+  NULL,NULL,NULL,NULL,
+  NULL,NULL,NULL,NULL,
+  18,17,16,15,
+  14,NULL,NULL,NULL,
+  };
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
