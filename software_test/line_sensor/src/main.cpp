6c990dc4b5fa44a3ead400b3085db2448a1a032e
hamakawa <hirame.tech@gmail.com>
2022-06-17 01:20:03 +0900

    update lineread

--- software_test/line_sensor/src/main.cpp
+++ software_test/line_sensor/src/main.cpp
@@ -1,16 +1,43 @@
 #include<Arduino.h>
+#include<Adafruit_NeoPixel.h>
 
-int censor_port[29] = {
-    //任意のポート番号を入力　値,値,値,・・・
-};
+#define BRIGHTNESS 200
+#define LED_PIN 6
 
-int censor_value[29] = {};
+Adafruit_NeoPixel strip(30, LED_PIN, NEO_GRB + NEO_KHZ800);
 
+void get_linesensor(int *pins,int *a_pins,int*value);
+
+int set_port[4] = {8,9,10,11};
+int read_port[2] = {A0,A1};
+
+int sensor_value[30];
+int old_value[30];
+void set_pin(){
+    for(int i = 0;i < 4; i++){
+        pinMode(set_port[i],OUTPUT);
+    }
+    pinMode(read_port[0],INPUT);//0~14
+    pinMode(read_port[1],INPUT);//15~30
+}
 void setup(){
     Serial.begin(9600);
+    strip.begin();           
+    strip.show();            // Turn OFF
+    strip.setBrightness(BRIGHTNESS);
+
 }
 
 void loop(){
+    for(int i = 0; i < 30; i++){
+        strip.setPixelColor(i, strip.Color(0,255,0));
+    }
+    strip.show();
+    
+    get_linesensor(set_port,read_port,sensor_value);
+    Serial.println((sensor_value[2]+old_value[2])/2);//簡易平滑化
+    old_value[2] = sensor_value[2];
+    /*
     for(int i=0; i < 30;i++){
         censor_value[i] = analogRead(censor_port[i]);
         Serial.print("censor");
@@ -19,7 +46,19 @@ void loop(){
         Serial.println(censor_value[i]);
         Serial.print("\n");
         delay(100);
+    }*/
+}
+
+void get_linesensor(int *pins,int *a_pins,int*value){
+    int i = 2;
+    for(int num=0; num < 4; num++){
+        if(bitRead(i,num) == 1){
+            digitalWrite(pins[num],HIGH);
+        }else{
+            digitalWrite(num,LOW);
+        }
     }
+    value[i] = analogRead(a_pins[0]);
 }
 
 //スケッチの動作確認済み
