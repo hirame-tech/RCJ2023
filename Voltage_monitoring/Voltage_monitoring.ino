664dad64912d47af8e0d8f2dce972541cf700473
hamakawa <hirame.tech@gmail.com>
2022-10-19 03:32:56 +0900

    add ATtiny Program

---
+++ Voltage_monitoring/Voltage_monitoring.ino
@@ -0,0 +1,28 @@
+#define SENSOR_PIN 1
+#define FET_PIN 7
+#define LED_PIN 0
+
+#define TRESHOLD 100
+
+void setup() {
+  pinMode(SENSOR_PIN,INPUT);
+  pinMode(FET_PIN,OUTPUT);
+  pinMode(LED_PIN,OUTPUT);
+}
+
+void loop() {
+  static int flag_trigger = 0;
+  int sensor_val = digitalRead(SENSOR_PIN);
+  if(sensor_val == 0){// Low Voltage
+    flag_trigger++;
+  }else{
+    flag_trigger = 0;
+  }
+  if(flag_trigger > TRESHOLD){
+    digitalWrite(FET_PIN,HIGH);//fet is negative
+    digitalWrite(LED_PIN,LOW);
+  }else{
+    digitalWrite(FET_PIN,LOW);//fet is active
+    digitalWrite(LED_PIN,HIGH);
+  }
+}
