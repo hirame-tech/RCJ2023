25ae40bf6a66081a3303b98ab8f83045fbba4fe6
hamakawa <hirame.tech@gmail.com>
2022-11-24 01:02:43 +0900

    些細な変更

--- software_test/Send_motor_speed/src/main.cpp
+++ software_test/Send_motor_speed/src/main.cpp
@@ -1,13 +1,14 @@
 #include <Arduino.h>
-
+//70~
 void setup() {
   Serial.begin(9600);
-  Serial1.begin(9600);
-  pinMode(A0,INPUT);
+  Serial2.begin(9600);
+  pinMode(A11,INPUT);
 }
 
 void loop() {
-  int val = map(analogRead(A0),0,1023,0,200);
+  int val = map(analogRead(A11),0,1023,0,255);
   Serial.println(val);
-  Serial1.write(val);
+  Serial2.write(val);
+  delay(1);
 }
