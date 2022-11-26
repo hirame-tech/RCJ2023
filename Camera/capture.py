2195f8d89e4907633e9b6bd16de83f72c5cb785e
siero1012 <jajanp4869@gmail.com>
2022-11-27 03:51:30 +0900

    コメントを追加

--- Camera/capture.py
+++ Camera/capture.py
@@ -3,17 +3,17 @@ import numpy as np
 camera=cv2.VideoCapture(0)
 blue=[(93,59,68),(166,255,255)] #[(lower),(upper)]
 
-def nichika(img,lower,upper):
+def nichika(img,lower,upper): #画像を二値化する引数は(画像、(hsvタプル),(hsvタプル))
     hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
     nichika=cv2.inRange(hsv,lower,upper)
     
     return nichika
 
-def addshape(binimg,img,w,h):
+def addshape(binimg,img,w,h): #引数は（二値化画像、マスク処理後画像、画像幅、画像高さ）
     nlabels,labels,stats,center=cv2.connectedComponentsWithStats(binimg)
     if nlabels!=1:
-        big=np.argmax(stats[1:,4])+1 #the label of biggest blob
-        cv2.rectangle(img,(stats[big][0],stats[big][1]),(stats[big][0]+stats[big][2],stats[big][1]+stats[big][3]),(0,255,0),thickness=10)
+        big=np.argmax(stats[1:,4])+1 #最も大きい塊のラベルを取得
+        cv2.rectangle(img,(stats[big][0],stats[big][1]),(stats[big][0]+stats[big][2],stats[big][1]+stats[big][3]),(0,255,0),thickness=10) #物体を長方形で示す
         angle=np.arctan2(h/2-center[big][1],center[big][0]-w/2) #物体の中心からの角度
         #cv2.putText(img,str(angle),int(center[big]),cv2.FONT_HERSHEY_SIMPLEX,1.0,(255,0,0),2)
         print(angle)
@@ -36,3 +36,4 @@ while True:
 
 camera.release()
 cv2.destroyAllWindows()
+#test
