3658f5b8604d8099a64d904a6d67550fbd9f3339
hirame-tech <101329221+hirame-tech@users.noreply.github.com>
2023-03-25 11:24:04 +0900

    add description

--- Camera/read_serial/read_serial.ino
+++ Camera/read_serial/read_serial.ino
@@ -3,16 +3,16 @@ void setup() {
 }
 
 void loop() {
-  int angleb;
-  int angley;
-  char lr;
-  char by;
+  int angleb;//angle of blue    0~7:angle(center:0) 8:can't detect
+  int angley;//angle of yellow
+  char lr;// x = can't detect l = left r = right
+  char by;// x = can't detect m = center b = blue y = yellow
   if (Serial.available() > 0) {
     String input = Serial.readStringUntil('\n');
     angleb = input[0] - '0';
     angley = input[1] - '0';
     lr = input[2];
     by = input[3];
-
+    
   }
 }
